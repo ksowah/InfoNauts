46b4ff7d66be95a346801d9c6717ed367f8e0bc2
ksowah <sowahkelvin640@gmail.com>
2023-08-29 09:40:12 +0000

    update goals

--- components/Goals.tsx
+++ components/Goals.tsx
@@ -15,12 +15,12 @@ const Goals = () => {
         <div className="flex-1 h-[22rem] rounded-xl bg-primary p-[1.5rem] " >
             <h2 className="font-extrabold text-[1.8rem] " >GOALS</h2>
             <div className="pl-[1rem] space-y-4" >
-                <p className="text-[1.2rem]" >- Align Education with Industry Needs</p>
-                <p className="text-[1.2rem]" >- Empower Informed Decision-Making</p>
-                <p className="text-[1.2rem]" >- Enhance Student Employability</p>
-                <p className="text-[1.2rem]" >- Optimize Resource Allocation</p>
-                <p className="text-[1.2rem]" >- Bridge Education-Employability Gap</p>
-                <p className="text-[1.2rem]" >- Transform Education Paradigms</p>
+                <p className="text-[1.14rem]" >- Understand enrollment trends and patterns to allocate resources effectively.</p>
+                <p className="text-[1.14rem]" >- Identify age groups and districts with the highest enrollment in various education levels.</p>
+                <p className="text-[1.14rem]" >- Develop strategies for accommodating expected enrollment shifts.</p>
+                <p className="text-[1.14rem]" >- Assess alignment between university programs and industry demands.</p>
+                <p className="text-[1.14rem]" >- Determine if graduates are entering occupations relevant to their field of study.</p>
+                <p className="text-[1.14rem]" >- Inform curriculum updates to ensure programs remain industry-relevant.</p>
             </div>
         </div>
        
